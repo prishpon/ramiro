6067c33a3380393c2edf566fb19570c38a07f372
prishpon <prishpon@gmail.com>
2024-08-28 17:19:35 +0200

    Loading  gutenberg editor styles

--- functions.php
+++ functions.php
@@ -5,7 +5,10 @@
 // Includes
 include(get_theme_file_path('/includes/front/enqueue.php'));
 include(get_theme_file_path('/includes/front/head.php'));
+include(get_theme_file_path('/includes/setup.php'));
+
 
 // Hooks
 add_action('wp_enqueue_scripts', 'rmr_enqueue');
 add_action('wp_head', 'rmr_head', 5);
+add_action( 'after_setup_theme', 'rmr_setup_theme' );
